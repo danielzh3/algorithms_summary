17083f068e42477a69bbd7bf7b6a130a57a1cfe9
danielzh <danielzh@DZMBP.T-mobile.com>
2017-06-27 22:16:34 -0700

    add sort for cpp

---
+++ cpp/sort.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+template<class T>
+class Sorting_algorithm{
+public:
+	vector<T> merge_sort(vector<T> input);
+
+};
+
+
+
+vector<T> Sorting_algrithm::merge_sort(vector<T>){
+	
+}
